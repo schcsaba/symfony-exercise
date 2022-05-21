b44ddedbd3c70a37b2c054e9be7130736760cb6d
Csaba Schnitchen <schcsaba@gmail.com>
2022-05-21 20:39:34 +0200

    Customize Register form

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,17 +4,49 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('password')
-        ;
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'johndoe'
+                ],
+                'required' => true,
+                'constraints' => new Length([
+                    'min' => 3,
+                    'max' => 255
+                ])
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password and the confirmation must be identical',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'placeholder' => 'Please type your password'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirm your password',
+                    'attr' => [
+                        'placeholder' => 'Please confirm your password'
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Register'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
