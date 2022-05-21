b1c1f863ae8ddb44663b6deaa7c303dafee6bc25
Csaba Schnitchen <schcsaba@gmail.com>
2022-05-21 22:37:25 +0200

    Save user data to database in RegisterController

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,22 +4,40 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     /**
-     * @Route("/inscription", name="register_index")
+     * @Route("/register", name="register_index")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
-        $form = $this->createForm(RegisterType::class, $user);
+        $registerForm = $this->createForm(RegisterType::class, $user);
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+            $user = $registerForm->getData();
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
 
         return $this->render('register/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $registerForm->createView()
         ]);
     }
 }
